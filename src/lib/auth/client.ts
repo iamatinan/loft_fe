394743fda35ba21e35c1fe243ca5f9352fbac91b
Atinanz <atinan.rakhom@gmail.com>
2025-06-28 19:21:19 +0700

    fix: add disble lint

--- src/lib/auth/client.ts
+++ src/lib/auth/client.ts
@@ -2,21 +2,12 @@
 
 import type { User } from '@/types/user';
 import api from '@/utils/api';
-import React from 'react';
 
 function generateToken(): string {
   const arr = new Uint8Array(12);
   window.crypto.getRandomValues(arr);
   return Array.from(arr, (v) => v.toString(16).padStart(2, '0')).join('');
 }
-
-const user = {
-  id: 'USR-000',
-  avatar: '/assets/avatar.png',
-  firstName: 'Sofia',
-  lastName: 'Rivers',
-  email: 'sofia@devias.io',
-} satisfies User;
 
 export interface SignUpParams {
   firstName: string;
